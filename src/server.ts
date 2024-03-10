ec27b201fc7d394e6f271921382765d4bbba6bda
Gabriel Resende <gabriel@sensio.com.br>
2024-03-10 14:15:01 -0300

    feat(server): set error handler/corsOptions

--- src/server.ts
+++ src/server.ts
@@ -13,13 +13,36 @@ connect();
 
 const app = express();
 
-app.use(express.json());
-app.use(routes);
-app.use(cors());
+const errorHandler = (err, req, res, next) => {
+    console.error(err.message, err.stack);
 
-app.use((error: any, request: any, response: any, next: any) => {
-    response.status(500).json({ error: error.message });
-});
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    const response = {
+        success: false,
+        error: {
+            message,
+            code: err.code,
+        },
+    };
+
+    res.status(statusCode).json(response);
+};
+
+const corsOptions = (err, req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    next();
+}
+
+
+app.use(cors());
+app.use(routes);
+app.use(express.json());
+app.use(errorHandler);
+app.use(corsOptions);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
