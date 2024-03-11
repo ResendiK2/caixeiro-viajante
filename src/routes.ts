20ed4ed699f73060d766646fb7567389c31ea895
Gabriel Resende <gabriel@sensio.com.br>
2024-03-10 23:11:20 -0300

    feat(routes): add getRoute

--- src/routes.ts
+++ src/routes.ts
@@ -6,11 +6,13 @@ import {
     filterClientsController,
     getClientByIdController,
     listClientsController,
-    updateClients
+    updateClients,
+    getRoute
 } from './controllers';
 
 const routes = express.Router();
 
+routes.get('/clients/route', getRoute);
 routes.get('/clients', listClientsController);
 routes.get('/clients/:id', getClientByIdController);
 routes.post('/clients', createClients);
